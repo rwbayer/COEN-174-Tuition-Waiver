12e17dff62a88e5992b45bee61c76fa13361c455
Robert Bayer <robertwbayer@gmail.com>
2016-10-24 15:28:46 -0700

    Add course ui functionality

--- js/main.js
+++ js/main.js
@@ -2,13 +2,23 @@ function handleRadioClick(myRadio)
 {
 	if (myRadio.value == "ta")
 	{
-		// hide
 		$('#raFundingSource').hide();
+		$('#taPercentFTE').show();
 	}
 	else
 	{
-		// show
 		$('#raFundingSource').show();
-
+		$('#taPercentFTE').hide();
 	}
 }
+$(document).ready(function(e)
+{
+	// add another course section to the page
+	$("#addCourse").on("click", function(e)
+	{
+		// make the page not reload on button click
+		e.preventDefault();
+
+		$("#courses").append("<div class=\"form-group col-xs-12 col-sm-3\"><label for=\"cIdInput\">Course ID</label><input type=\"text\" class=\"form-control\" id=\"cIdInput\" placeholder=\"ex: MECH207\"></div><div class=\"form-group col-xs-12 col-sm-6\"><label for=\"courseInput\">Course Title</label><input type=\"text\" class=\"form-control\" id=\"courseInput\" placeholder=\"ex: Mechanical Systems\"></div><div class=\"form-group col-xs-12 col-sm-3\"><label for=\"creditInput\"># of Credits</label><input type=\"text\" class=\"form-control\" id=\"creditInput\" placeholder=\"ex: 4\"></div>");
+	});
+});
